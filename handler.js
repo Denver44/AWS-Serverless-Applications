6e83d9c1c7033a6dea094ed554d017f71ed4cfc4
Denver <70090559+Denver44@users.noreply.github.com>
2024-01-26 02:35:51 +0530

    All end points created

--- handler.js
+++ handler.js
@@ -6,3 +6,34 @@ module.exports.createNote = async (event) => {
     body: JSON.stringify('Note created successfully!'),
   };
 };
+
+module.exports.updateNote = async (event) => {
+  const { id } = event.pathParameters;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`Note with id: ${id} updated successfully!`),
+  };
+};
+
+module.exports.deleteNote = async (event) => {
+  const { id } = event.pathParameters;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`Note with id: ${id} deleted successfully!`),
+  };
+};
+
+module.exports.getNote = async (event) => {
+  const { id } = event.pathParameters;
+  return {
+    statusCode: 200,
+    body: JSON.stringify(`Note with id: ${id} fetched successfully!`),
+  };
+};
+
+module.exports.getAllNote = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify('All notes fetched successfully!'),
+  };
+};

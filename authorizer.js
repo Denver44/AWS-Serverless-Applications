8486257911b5084ecf360beb921c7d9d2ef2ede9
Denver <70090559+Denver44@users.noreply.github.com>
2024-02-10 18:56:55 +0530

    Learn About Authentication and Authorization

--- authorizer.js
+++ authorizer.js
@@ -1,4 +1,13 @@
-'use strict';
+const { CognitoJwtVerifier } = require('aws-jwt-verify');
+const COGNITO_USER_POOL_ID = process.env.COGNITO_USER_POOL_ID;
+const COGNITO_USER_POOL_WEB_CLIENT_ID =
+  process.env.COGNITO_USER_POOL_WEB_CLIENT_ID;
+
+const jwtVerifier = CognitoJwtVerifier.create({
+  userPoolId: COGNITO_USER_POOL_ID,
+  tokenUse: 'id',
+  clientId: COGNITO_USER_POOL_WEB_CLIENT_ID,
+});
 
 // This is IAM policy, not lambda policy so it will have mandatory fields like Version, Statement, Action, Effect, Resource
 //  To check the response of an IAM Policy go to IAM -> Policies -> And check code of any policy under Permissions tab
@@ -21,19 +30,24 @@ const generatePolicy = (principalId, effect, resource) => {
   // So after checking the token and if it is valid then we can pass some data to the lambda function
   // like user data or any other data which can be used in the lambda function
   authResponse.context = {
-    stringKey: 'stringval',
-    numberKey: 123,
-    booleanKey: true,
+    foo: 'bar',
   };
-  console.log(JSON.stringify(authResponse));
   return authResponse;
 };
 
-exports.handler = async (event, context, callback) => {
+module.exports.handlerV2 = async (event, context, callback) => {
+  const token = event.authorizationToken;
+  try {
+    await jwtVerifier.verify(token);
+    callback(null, generatePolicy('user', 'Allow', event.methodArn));
+  } catch (error) {
+    callback('Error: Invalid token ');
+  }
+};
+
+module.exports.handler = async (event, context, callback) => {
   // lambda authorizer function
-
-  var token = event.authorizationToken; // "allow" or "deny" are the only two options
-
+  const token = event.authorizationToken; // "allow" or "deny" are the only two options
   switch (token) {
     case 'allow':
       callback(null, generatePolicy('user', 'Allow', event.methodArn));

613f21a0c147de9e1b674f7e0e001b3d314c13b4
Denver <70090559+Denver44@users.noreply.github.com>
2025-06-29 16:25:57 +0530

    feat: implement CRUD operations for notes in handler and serverless configuration

--- handler.js
+++ handler.js
@@ -1,14 +1,52 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.createNote = async (event) => {
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      message: 'A new note created',
+    }),
+  };
+};
+
+module.exports.updateNote = async (event) => {
+  const noteId = event.pathParameters.id;
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Welcome to AWS Serverless Applications!',
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify({
+      message: `The note with id ${noteId} is updated`,
+    }),
+  };
+};
+
+module.exports.deleteNote = async (event) => {
+  const noteId = event.pathParameters.id;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `The note with id ${noteId} is deleted`,
+    }),
+  };
+};
+
+module.exports.getAllNotes = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'All notes are returned',
+    }),
+  };
+};
+
+module.exports.getNoteById = async (event) => {
+  const noteId = event.pathParameters.id;
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `The note with id ${noteId} is returned`,
+    }),
   };
 };
